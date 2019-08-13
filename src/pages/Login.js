cf041a8e6de6e9a54873159777d3278642ef548e
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-13 16:51:08 -0300

    Skipping logging if already logged with AsyncStorage and useEffect, a simple hack (cause Login blinks!)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { KeyboardAvoidingView, Platform, Text, StyleSheet, Image, TextInput, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
@@ -8,12 +9,20 @@ import logo from '../assets/logo.png';
 export default function Login({ navigation }) {
   const [user, setUser] = useState('');
 
+  useEffect(() => {
+    AsyncStorage.getItem('user').then(user => {
+      if (user) {
+        navigation.navigate('Main', { user })
+      }
+    });
+  }, []);
+
   async function handleLogin() {
     const response = await api.post('/devs', { username: user });
 
     const { _id } = response.data;
 
-    console.log(_id);
+    await AsyncStorage.setItem('user', _id);
 
     navigation.navigate('Main', { _id });
   }
