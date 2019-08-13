13decb8d818350d29e3fc1b53dc259c372a8dfff
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-13 15:55:07 -0300

    Change to switch navigator again

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
 export default createAppContainer(
-  createStackNavigator({
+  createSwitchNavigator({
     Login,
     Main,
   })
