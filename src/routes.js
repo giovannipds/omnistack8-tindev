7588e56fa6471c650c82a000a4823265e0ee2c72
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-13 15:55:47 -0300

    Create bottom tab navigator

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
 import Main from './pages/Main';
 
 export default createAppContainer(
-  createSwitchNavigator({
+  createBottomTabNavigator({
     Login,
     Main,
   })
