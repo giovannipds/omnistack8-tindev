de41ae019a116a50f81b572a58bf97292a79d7ef
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-13 15:10:26 -0300

    Empty TextInputs don't show

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TextInput } from 'react-native';
 
 import logo from '../assets/logo.png';
 
@@ -7,6 +7,8 @@ export default function Login() {
   return (
     <View style={styles.container}>
       <Image source={logo} />
+
+      <TextInput />
     </View>
   );
 }
