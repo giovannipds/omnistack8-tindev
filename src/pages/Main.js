d71b7132d46bfb8a96414380b2de10ed2ef53814
Giovanni Pires da Silva <giovannipds@gmail.com>
2019-08-18 10:33:30 -0300

    Change View to SafeAreaView

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { SafeAreaView, Image } from 'react-native';
 
 import logo from '../assets/logo.png';
 
 export default function Main() {
   return (
-    <View>
+    <SafeAreaView>
       <Image source={logo} />
-    </View>
+    </SafeAreaView>
   );
 }
